2ea6f5a3bde83a1b71bd6b7ffe9bcb4945c4e20e
Dhamodharan Krishnan <dhamukrish@gmail.com>
2020-04-26 11:35:32 +0200

    Updated with scripts

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,12 @@ FROM golang:alpine
 RUN mkdir /app
 COPY . /app
 WORKDIR /app
+
+RUN apk add git
+RUN go get github.com/gorilla/mux
+
 RUN go build -o main . 
 
-CMD ["/app/main"]
+EXPOSE 8080
+
+ENTRYPOINT ["/app/main"]
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -10,10 +11,20 @@ import (
 func CreateEndpoint(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(200)
 	w.Write([]byte("Item Created"))
+	fmt.Println("Accessed /create")
+}
+
+func HealthCheck(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
 }
 
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/create", CreateEndpoint).Methods("GET")
-	log.Fatal(http.ListenAndServe("localhost:8080", router))
+	router.HandleFunc("/health", HealthCheck).Methods("GET")
+
+	fmt.Println("Server is starting on http://:8080/create")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
